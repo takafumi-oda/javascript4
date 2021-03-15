9cd7c1dad9dc2c2355fb9a77b2ea0bd947ddd2a9
EC2 Default User <ec2-user@ip-172-31-10-198.ap-northeast-1.compute.internal>
2021-03-15 21:28:08 +0000

    javascript課題４提出

---
+++ javascript4.js
@@ -0,0 +1,6 @@
+ $('.slider').slick({
+   autoplay: true,
+   autoplaySpeed: 4000,
+   prevArrow: '<button class="slider-arrow prev-arrow">＜</button>',
+   nextArrow: '<button class="slider-arrow next-arrow">＞</button>',
+ });
